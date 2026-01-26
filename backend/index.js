5efcbc1253c6b304f4bf67edb5d906e2d5ebb714
Isaiah Chastain <ipchastain42@tntech.edu>
2026-01-26 12:34:05 -0600

    Added get all scanners routes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
+<<<<<<< HEAD
 import cors from "cors";
 import homeRouter from "./routes/homeRoutes.js";
+=======
+>>>>>>> 7f642ec (Added get all scanners routes)
 
 dotenv.config({ path: "../.env" });
 
@@ -11,7 +14,10 @@ const prisma = new PrismaClient();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
+<<<<<<< HEAD
 app.use(cors());
+=======
+>>>>>>> 7f642ec (Added get all scanners routes)
 
 async function connectToDatabase() {
   try {
@@ -27,4 +33,16 @@ app.listen(port, () => {
   connectToDatabase();
 });
 
+<<<<<<< HEAD
 app.use("/", homeRouter);
+=======
+
+app.get("/", async (req, res) => {
+    try {
+        const scanners = await prisma.scanner.findMany();
+        res.status(200).json(scanners);
+    } catch (err) {
+        res.status(500).json({success: false, error: err})
+    }
+})
+>>>>>>> 7f642ec (Added get all scanners routes)
