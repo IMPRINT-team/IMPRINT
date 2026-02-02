b4e18fb358c718f170ba6702510d8dd0f756a2b7
Isaiah Chastain <ipchastain42@tntech.edu>
2026-02-02 12:08:01 -0600

    Added layout

--- web-dashboard/src/pages/Dashboard.jsx
+++ web-dashboard/src/pages/Dashboard.jsx
@@ -5,14 +5,14 @@ import HeroPanel from "../components/dashboard/HeroPanel.jsx"
 import NavBar from "../components/dashboard/NavBar.jsx"
 
 const Dashboard = () => (
-  <div className="min-h-screen bg-void px-6 py-6 text-slate-300">
+  <div className="min-h-screen bg-void px-6 py-6 text-slate-300 lg:h-svh">
     <div className="flex h-full flex-col gap-6">
       <header className="lg:h-[12vh]">
         <NavBar />
       </header>
       <main className="flex-1">
         <div className="grid h-full grid-cols-1 gap-6 lg:grid-cols-12">
-          <section className="order-1 flex flex-col gap-6 lg:col-span-8 lg:grid lg:grid-rows-[2fr_1fr]">
+          <section className="order-1 flex flex-col gap-6 lg:col-span-8 lg:grid lg:grid-rows-[4fr_1fr]">
             <HeroPanel />
             <BoxA />
           </section>
